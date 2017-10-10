6dbd7f246d20817a06d724a342cf61c0e06b6a8a
FelixdeJonge <felixdejonge@gmail.com>
2017-10-10 11:30:34 +0200

    Zoeken is nu weer kapot

--- searcher.js
+++ searcher.js
@@ -8,7 +8,6 @@ class ProductLink {
         return "<a href= " + this.pagelink + "><img src=" + this.imagelink + " /><h2></br>" + this.name + "</h2> Naar Product</a>";
     }
 }
-var array1 = [new ProductLink("DOOM", "\"Images/doom-cover-new.jpg\"", "\"ConsoleChoiceDoom.html\"")];
-// document.getElementById("searchres").innerHTML = array1[0].makelink();
-document.write(array1[0].makelink());
+var array1 = [new ProductLink("DOOM", "\"Images/doom-cover-new.jpg\"", "\"ConsoleChoiceDoom.html\""), new ProductLink("Call of Duty: Advanced Warfare", "\"Images/COD-AW-cover.jpg\"", "\"ConsoleChoiceAW.html\""), new ProductLink("PlayerUnknown's Battlegrounds", "\"Images/PUBG-cover.jpg\"", "\"ConsoleChoiceBATTLE.html\""), new ProductLink("Xbox console", "\"Images/Xbox.jpeg\"", "\"XBoX.html\"")];
+document.getElementById("search1").innerHTML = array1[0].makelink();
 //# sourceMappingURL=searcher.js.map
--- searcher.ts
+++ searcher.ts
@@ -9,11 +9,15 @@
         this.pagelink = p;
     }
     makelink() {
-        return "<a href= " + this.pagelink + "><img src=" + this.imagelink + " /><h2></br>" + this.name + "</h2> Naar Product</a>";
+        return "<a href= " + this.pagelink + " ><img src=" + this.imagelink + " /><h2></br>" + this.name + "</h2>Naar Product</a>";
     }
 }
 
-var array1 = [new ProductLink("DOOM", "\"Images/doom-cover-new.jpg\"", "\"ConsoleChoiceDoom.html\"")]
+var array1 = [new ProductLink("DOOM", "\"Images/doom-cover-new.jpg\"", "\"ConsoleChoiceDoom.html\""), new ProductLink("Call of Duty: Advanced Warfare", "\"Images/COD-AW-cover.jpg\"", "\"ConsoleChoiceAW.html\""), new ProductLink("PlayerUnknown's Battlegrounds", "\"Images/PUBG-cover.jpg\"", "\"ConsoleChoiceBATTLE.html\""), new ProductLink("Xbox console", "\"Images/Xbox.jpeg\"", "\"XBoX.html\"")]
+document.getElementById("search1").innerHTML = array1[0].makelink();
 
-// document.getElementById("searchres").innerHTML = array1[0].makelink();
-document.write(array1[0].makelink());
+document.getElementById("search2").innerHTML = array1[1].makelink();
+
+document.getElementById("search3").innerHTML = array1[2].makelink();
+
+document.getElementById("search4").innerHTML = array1[3].makelink();
