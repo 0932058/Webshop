1fc53137487b4890e2fba9584006f988c42634b1
FelixdeJonge <felixdejonge@gmail.com>
2017-10-11 14:48:38 +0200

    Wenslijst beginnetje

--- ReactWebshop/ClientApp/components/Wenslijst.tsx
+++ ReactWebshop/ClientApp/components/Wenslijst.tsx
@@ -5,11 +5,17 @@ interface WinkelmandState {
     Storage: string | null;
 }
 
-class codGame{
+class Product{
+    name: string;
     price: Number;
+    image: string;
+    link: string;
 
-    constructor(){
-        this.price = 50;
+    constructor(name : string, price : Number, image : string, link : string){
+        this.name = name;
+        this.price = price;
+        this.image = image;
+        this.link = link;
     }
 }
 
@@ -17,9 +23,7 @@ export class Wenslijst extends React.Component<RouteComponentProps<{}>, Winkelma
     constructor(){
         super();
 
-        var game = new codGame();
-
-
+        var game;
         this.add = this.add.bind(this);
         this.retrieve = this.retrieve.bind(this);
         localStorage.setItem('wenslijst', JSON.stringify({list: [game] }));
@@ -37,21 +41,48 @@ export class Wenslijst extends React.Component<RouteComponentProps<{}>, Winkelma
         return this.state.Storage;
     }
 
+    makeTable(array : Array<Product>){
+        array.forEach(Product => {
+            <div className="CartProduct">
+            <a href={ Product.link } ><img src={ Product.image }/></a>
+            <h2 className="CartProductPart">{ Product.name }</h2>
+            <a href={ Product.link } className="CartProductPart">Naar product</a>           
+            <button href="Winkelmand.html">In winkelmand</button>
+            <button className="CartProductPart">Verwijderen</button>
+
+        </div>
+        });
+    }
+
     public render() {
 
+        //hier wordt de value van wenslijst naar string gecast om vervolgens naar een JSON object te worden gecast
         var voorbeeld = JSON.parse(String(localStorage.getItem('wenslijst')));
 
+        
+        //zo pas je een attribuut van een instantie in de lijst aan
         voorbeeld.list[0].price = 20;
 
+        //hier wordt de lijst weer teruggezet naar de localstorage
         localStorage.setItem('wenslijst', JSON.stringify(voorbeeld));
         
         voorbeeld = JSON.parse(String(localStorage.getItem('wenslijst')));
 
-        return <div>
+        return <section>
+    
+        <h1>Wenslijst</h1>
+ 
+        <div className="CartProduct">
             
-                <div>
-                    <h1>{ voorbeeld.list[0].price }</h1>
-                </div>
-            </div>;
+                <a href= ><img src= style="height: 15em;"/></a>
+            <h2 className="CartProductPart">Doom 2016</h2>
+            <a href="Product1pagina" className="CartProductPart">Naar product</a>           
+            <a href="Winkelmand.html">In winkelmand</a>
+            <button className="CartProductPart">Verwijderen</button>
+
+        </div>
+        
+
+    </section>;
     }
 }
