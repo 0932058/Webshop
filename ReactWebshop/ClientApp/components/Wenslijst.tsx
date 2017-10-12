4040da52910c62a58829d7fe4c7984d42369ebc5
FelixdeJonge <felixdejonge@gmail.com>
2017-10-12 10:31:18 +0200

    Wenslijst word nu getekent

--- ReactWebshop/ClientApp/components/Wenslijst.tsx
+++ ReactWebshop/ClientApp/components/Wenslijst.tsx
@@ -1,88 +1,114 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import Img from 'react-image';
 
-interface WinkelmandState {
-    Storage: string | null;
+interface WenslijstState{ 
+    games: WenslijstGame[] 
 }
 
-class Product{
-    name: string;
-    price: Number;
-    image: string;
-    link: string;
-
-    constructor(name : string, price : Number, image : string, link : string){
-        this.name = name;
-        this.price = price;
-        this.image = image;
-        this.link = link;
-    }
+type WenslijstGame = {
+    name:string,
+    console: string,
+    price: number,
+    category:string,
+    orderdate: number | null,
+    status: string | null,
+    image: string
 }
 
-export class Wenslijst extends React.Component<RouteComponentProps<{}>, WinkelmandState> {
+export class Wenslijst extends React.Component<RouteComponentProps<{}>, WenslijstState> {
     constructor(){
         super();
+        this.createStorageAndState = this.createStorageAndState.bind(this);
+        this.ConvertJson = this.ConvertJson.bind(this);
+        this.addGameToStorage = this.addGameToStorage.bind(this);
+        this.removeGameFromStorage = this.removeGameFromStorage.bind(this);
+        this.state = {games:[]}
+        
+    }
+    addGameToStorage() {
+        var games = JSON.parse(String(localStorage.getItem('Wenslijst')));
+        var gamesList: WenslijstGame[] = games.list;
+        var game: WenslijstGame = {name: "GTA V", console: "Xbox", price: 60, category: "Games", orderdate: Date.now(), status:"Onderweg",
+        image: "http://gamesofpc.com/wp-content/uploads/2015/02/GTA-5-Download.jpg"}
+        
+        var game2: WenslijstGame = {name: "Call of duty Infinite Warfare",console: "Xbox ", price: 55, category: "Games", orderdate: Date.now(), status:"Bezorgd",
+        image: "https://www.gamestop.com/common/images/lbox/125879b.jpg"}
+        
+        var game3: WenslijstGame = {name: "GTA 3",console: "Playstation 2", price: 50, category: "Games", orderdate: Date.now(), 
+       status:"Onderweg", image:"https://www.lukiegames.com/assets/images/XBOX/xbox_grand_theft_auto_iii-110214.jpg"}
+        
+       var game4: WenslijstGame = {name: "Fifa 16",console: "Xbox", price: 55, category: "Games", orderdate: Date.now(),
+        status:"Onderweg", image:"https://www.instant-gaming.com/images/products/836/271x377/836.jpg"}
+        
+        gamesList.push(game);
+        gamesList.push(game2);
+        gamesList.push(game3);
+        gamesList.push(game4);
 
-        var game;
-        this.add = this.add.bind(this);
-        this.retrieve = this.retrieve.bind(this);
-        localStorage.setItem('wenslijst', JSON.stringify({list: [game] }));
+        localStorage.setItem('Wenslijst', JSON.stringify({list: gamesList }));
+        
+        this.ConvertJson();
     }
 
-    add(){
-        var Storage = localStorage.getItem("works");
+    removeGameFromStorage(name: String){
+        var games = JSON.parse(String(localStorage.getItem('Wenslijst')));
+        var gamesList: WenslijstGame[] = games.list;
 
-        this.setState({
-            Storage
+        var newList: WenslijstGame[] = []; 
+
+        gamesList.map(item => {
+            if(item.name !== name){
+                newList.push(item);
+            }
         });
+
+        localStorage.setItem('Wenslijst', JSON.stringify({list: newList }));
+        
+        this.ConvertJson();
     }
 
-    retrieve(){
-        return this.state.Storage;
+    createStorageAndState(){
+        var game: WenslijstGame = {name: "GTA V", console: "Xbox", price: 60, category: "Games", orderdate: Date.now(), status:"Onderweg",
+        image: "http://gamesofpc.com/wp-content/uploads/2015/02/GTA-5-Download.jpg"}
+        
+        var game2: WenslijstGame = {name: "Call of duty Infinite Warfare",console: "Xbox ", price: 55, category: "Games", orderdate: Date.now(), status:"Bezorgd",
+        image: "https://www.gamestop.com/common/images/lbox/125879b.jpg"}
+        
+        var game3: WenslijstGame = {name: "GTA 3",console: "Playstation 2", price: 50, category: "Games", orderdate: Date.now(), 
+       status:"Onderweg", image:"https://www.lukiegames.com/assets/images/XBOX/xbox_grand_theft_auto_iii-110214.jpg"}
+        
+       var game4: WenslijstGame = {name: "Fifa 16",console: "Xbox", price: 55, category: "Games", orderdate: Date.now(),
+        status:"Onderweg", image:"https://www.instant-gaming.com/images/products/836/271x377/836.jpg"}
+        
+        localStorage.setItem('Wenslijst', JSON.stringify({list: [game, game2] }));
     }
 
-    makeTable(array : Array<Product>){
-        array.forEach(Product => {
-            <div className="CartProduct">
-            <a href={ Product.link } ><img src={ Product.image }/></a>
-            <h2 className="CartProductPart">{ Product.name }</h2>
-            <a href={ Product.link } className="CartProductPart">Naar product</a>           
-            <button href="Winkelmand.html">In winkelmand</button>
-            <button className="CartProductPart">Verwijderen</button>
-
-        </div>
-        });
-    }
-
-    public render() {
-
+    ConvertJson() : any{
         //hier wordt de value van wenslijst naar string gecast om vervolgens naar een JSON object te worden gecast
-        var voorbeeld = JSON.parse(String(localStorage.getItem('wenslijst')));
+        var games = JSON.parse(String(localStorage.getItem('Wenslijst')));    
+        this.setState({games: games.list})
+    }
 
-        
-        //zo pas je een attribuut van een instantie in de lijst aan
-        voorbeeld.list[0].price = 20;
+    componentWillMount(){
+        this.createStorageAndState();
+        this.ConvertJson();
 
-        //hier wordt de lijst weer teruggezet naar de localstorage
-        localStorage.setItem('wenslijst', JSON.stringify(voorbeeld));
-        
-        voorbeeld = JSON.parse(String(localStorage.getItem('wenslijst')));
+    }
 
-        return <section>
+    render() {
+        return <div className={"Wenslijst"}>
+                <h1>Wenslijst</h1>
+                {this.state.games.map(game => (
+                  <div>
+                   <li> <img src={game.image}  height={300}/> </li>
+                   <div> <h2> {game.name} </h2> </div>
+                   <div> <h2> Prijs: {game.price} </h2></div>
+                   <div> _________________________ </div>
+                   </div>
+                ))}
+            </div>;
     
-        <h1>Wenslijst</h1>
- 
-        <div className="CartProduct">
-            {/* Commented out because of an error - Stefan}
-             {/* <a href= ""><img src={} style="height: 15em;"/></a> </img> */}
-            <h2 className="CartProductPart">Doom 2016</h2>
-            <a href="Product1pagina" className="CartProductPart">Naar product</a>           
-            <a href="Winkelmand.html">In winkelmand</a>
-            <button className="CartProductPart">Verwijderen</button>
-
-        </div>
-        
-
-    </section>;
     }
 }
+
