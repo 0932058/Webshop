b5e26559c78edffe8eaa8e3625e8dc5982b05ecf
FelixdeJonge <felixdejonge@gmail.com>
2017-10-12 12:42:56 +0200

    Wenslijst gelinkt aan Winkelmand

--- ReactWebshop/ClientApp/components/Wenslijst.tsx
+++ ReactWebshop/ClientApp/components/Wenslijst.tsx
@@ -3,10 +3,10 @@ import { RouteComponentProps } from 'react-router';
 import Img from 'react-image';
 
 interface WenslijstState{ 
-    games: WenslijstGame[] 
+    games: BestellingenGame[] 
 }
 
-type WenslijstGame = {
+type BestellingenGame = {
     name:string,
     console: string,
     price: number,
@@ -28,17 +28,17 @@ export class Wenslijst extends React.Component<RouteComponentProps<{}>, Wenslijs
     }
     addGameToStorage() {
         var games = JSON.parse(String(localStorage.getItem('Wenslijst')));
-        var gamesList: WenslijstGame[] = games.list;
-        var game: WenslijstGame = {name: "DOOM", console: "Xbox", price: 60, category: "Games", orderdate: Date.now(), status:"Onderweg",
+        var gamesList: BestellingenGame[] = games.list;
+        var game: BestellingenGame = {name: "DOOM", console: "Xbox", price: 60, category: "Games", orderdate: Date.now(), status:"Onderweg",
         image: "Images/doom-cover-new.jpg"}
         
-        var game2: WenslijstGame = {name: "Call of duty Infinite Warfare",console: "Xbox ", price: 55, category: "Games", orderdate: Date.now(), status:"Bezorgd",
+        var game2: BestellingenGame = {name: "Call of duty Infinite Warfare",console: "Xbox ", price: 55, category: "Games", orderdate: Date.now(), status:"Bezorgd",
         image: "https://www.gamestop.com/common/images/lbox/125879b.jpg"}
         
-        var game3: WenslijstGame = {name: "GTA 3",console: "Playstation 2", price: 50, category: "Games", orderdate: Date.now(), 
+        var game3: BestellingenGame = {name: "GTA 3",console: "Playstation 2", price: 50, category: "Games", orderdate: Date.now(), 
        status:"Onderweg", image:"https://www.lukiegames.com/assets/images/XBOX/xbox_grand_theft_auto_iii-110214.jpg"}
         
-       var game4: WenslijstGame = {name: "Fifa 16",console: "Xbox", price: 55, category: "Games", orderdate: Date.now(),
+       var game4: BestellingenGame = {name: "Fifa 16",console: "Xbox", price: 55, category: "Games", orderdate: Date.now(),
         status:"Onderweg", image:"https://www.instant-gaming.com/images/products/836/271x377/836.jpg"}
         
         gamesList.push(game);
@@ -53,9 +53,9 @@ export class Wenslijst extends React.Component<RouteComponentProps<{}>, Wenslijs
 
     removeGameFromStorage(name: String){
         var games = JSON.parse(String(localStorage.getItem('Wenslijst')));
-        var gamesList: WenslijstGame[] = games.list;
+        var gamesList: BestellingenGame[] = games.list;
 
-        var newList: WenslijstGame[] = []; 
+        var newList: BestellingenGame[] = []; 
 
         gamesList.map(item => {
             if(item.name !== name){
@@ -65,6 +65,24 @@ export class Wenslijst extends React.Component<RouteComponentProps<{}>, Wenslijs
 
         localStorage.setItem('Wenslijst', JSON.stringify({list: newList }));
         
+        this.ConvertJson();
+    }
+
+    addToShoppingCart(name : string){
+        var Winkelmand = JSON.parse(String(localStorage.getItem('winkelmand')));
+        var Wenslijst = JSON.parse(String(localStorage.getItem('Wenslijst')));
+
+        var WenslijstList: BestellingenGame[] = Wenslijst.list;
+        var WinkelmandList: BestellingenGame[] = Winkelmand.list;
+        
+        WenslijstList.map(item => {
+            if(item.name === name)
+            {WinkelmandList.push(item);}
+        });
+
+        localStorage.setItem('winkelmand', JSON.stringify({list: WinkelmandList}));
+        
+        this.removeGameFromStorage(name);
         this.ConvertJson();
     }
 
@@ -98,6 +116,7 @@ export class Wenslijst extends React.Component<RouteComponentProps<{}>, Wenslijs
                    <div> <h2> {game.name} </h2> </div>
                    <div> <h2> Prijs: {game.price} </h2></div>
                    <button onClick={() => {this.removeGameFromStorage(game.name)} }> remove this item</button>
+                   <button onClick={() => {this.addToShoppingCart(game.name)}}>Toevoegen aan Winkelmand</button>
                    </div>
                 ))}
             </div>;
