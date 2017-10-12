fa4f407ce20c22b5dfe946b1c20d403537773752
OlafvdGraaf <0928885@hr.nl>
2017-10-12 11:20:27 +0200

    added winkelmand to bestellingen

--- ReactWebshop/ClientApp/components/Bestellingen.tsx
+++ ReactWebshop/ClientApp/components/Bestellingen.tsx
@@ -6,7 +6,7 @@ interface BestellingenState{
     games: BestellingenGame[] 
 }
 //React reacts bad to self created classes, that is why a type is created
-type BestellingenGame = {
+export type BestellingenGame = {
     name:string,
     console: string,
     price: number,
@@ -25,15 +25,6 @@ export class Bestellingen extends React.Component<RouteComponentProps<{}>, Beste
     }
     //Adds games to the local storage
     CreateGamesAndLocalStorage(){
-        var game: BestellingenGame = {name: "GTA V", console: "Xbox", price: 60, category: "Games", orderdate: Date.now(), status:"Onderweg",
-         image: "http://gamesofpc.com/wp-content/uploads/2015/02/GTA-5-Download.jpg"}
-        var game2: BestellingenGame = {name: "Call of duty Infinite Warfare",console: "Xbox ", price: 55, category: "Games", orderdate: Date.now(), status:"Bezorgd",
-         image: "https://www.gamestop.com/common/images/lbox/125879b.jpg"}
-        var game3: BestellingenGame = {name: "GTA 3",console: "Playstation 2", price: 50, category: "Games", orderdate: Date.now(), 
-        status:"Onderweg", image:"https://www.lukiegames.com/assets/images/XBOX/xbox_grand_theft_auto_iii-110214.jpg"}
-        var game4: BestellingenGame = {name: "Fifa 16",console: "Xbox", price: 55, category: "Games", orderdate: Date.now(),
-         status:"Onderweg", image:"https://www.instant-gaming.com/images/products/836/271x377/836.jpg"}
-        localStorage.setItem('bestellingen', JSON.stringify({list: [game,game2,game3,game4] })); 
     }
     ConvertJson() : any{
         //hier wordt de value van wenslijst naar string gecast om vervolgens naar een JSON object te worden gecast
@@ -42,7 +33,7 @@ export class Bestellingen extends React.Component<RouteComponentProps<{}>, Beste
     }
     //This will get called when the component gets mounted (loaded)
     componentWillMount(){
-        this.CreateGamesAndLocalStorage();
+        //this.CreateGamesAndLocalStorage();
         this.ConvertJson()
 
     }
--- ReactWebshop/ClientApp/components/Winkelmand.tsx
+++ ReactWebshop/ClientApp/components/Winkelmand.tsx
@@ -1,20 +1,12 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { BestellingenGame } from './Bestellingen'
 
 interface WinkelmandProps{
 }
 
 interface WinkelmandState{ 
-    games: codGame[] 
-}
-class codGame{
-    price: number
-    name: String
-
-    constructor(name: String){
-        this.price = 50;
-        this.name = name;
-    }
+    games: BestellingenGame[] 
 }
 
 export class Winkelmand extends React.Component<RouteComponentProps<{}>, WinkelmandState> {
@@ -24,33 +16,57 @@ export class Winkelmand extends React.Component<RouteComponentProps<{}>, Winkelm
         this.ConvertJson = this.ConvertJson.bind(this);
         this.addGameToStorage = this.addGameToStorage.bind(this);
         this.removeGameFromStorage = this.removeGameFromStorage.bind(this);
+        this.addWinkelmandToBestellingen = this.addWinkelmandToBestellingen.bind(this);
 
         this.state = {games:[]}
-        
     }
 
     addGameToStorage() {
         var games = JSON.parse(String(localStorage.getItem('winkelmand')));
-        var gamesList: codGame[] = games.list;
-
-        gamesList.push(new codGame("Destiny 2"))
+        var gamesList: BestellingenGame[] = games.list;
         
-        var game = new codGame("cod");
-        var game2 = new codGame("pubg");
+        var game: BestellingenGame = {name: "GTA V", console: "Xbox", price: 60, category: "Games", orderdate: Date.now(), status:"Onderweg",
+        image: "http://gamesofpc.com/wp-content/uploads/2015/02/GTA-5-Download.jpg"}
+       var game2: BestellingenGame = {name: "Call of duty Infinite Warfare",console: "Xbox ", price: 55, category: "Games", orderdate: Date.now(), status:"Bezorgd",
+        image: "https://www.gamestop.com/common/images/lbox/125879b.jpg"}
+       var game3: BestellingenGame = {name: "GTA 3",console: "Playstation 2", price: 50, category: "Games", orderdate: Date.now(), 
+       status:"Onderweg", image:"https://www.lukiegames.com/assets/images/XBOX/xbox_grand_theft_auto_iii-110214.jpg"}
+       var game4: BestellingenGame = {name: "Fifa 16",console: "Xbox", price: 55, category: "Games", orderdate: Date.now(),
+        status:"Onderweg", image:"https://www.instant-gaming.com/images/products/836/271x377/836.jpg"}
 
         gamesList.push(game);
         gamesList.push(game2);
+        gamesList.push(game3);
+        gamesList.push(game4);
 
         localStorage.setItem('winkelmand', JSON.stringify({list: gamesList }));
         
         this.ConvertJson();
     }
 
+    addWinkelmandToBestellingen() {
+        localStorage.setItem('bestellingen', JSON.stringify({list: []}));
+        var bestellingen = JSON.parse(String(localStorage.getItem('bestellingen')));
+        var winkelmand = JSON.parse(String(localStorage.getItem('winkelmand')));
+
+        var winkelmandList: BestellingenGame[] = winkelmand.list;
+        var bestellingenList: BestellingenGame[] = bestellingen.list;
+        
+        winkelmandList.map(item => {
+            bestellingenList.push(item);
+        });
+
+        localStorage.setItem('bestellingen', JSON.stringify({list: bestellingenList}));
+
+        localStorage.setItem('winkelmand', JSON.stringify({list: []}));
+        this.ConvertJson();
+    }
+
     removeGameFromStorage(name: String){
         var games = JSON.parse(String(localStorage.getItem('winkelmand')));
-        var gamesList: codGame[] = games.list;
+        var gamesList: BestellingenGame[] = games.list;
 
-        var newList: codGame[] = []; 
+        var newList: BestellingenGame[] = []; 
 
         gamesList.map(item => {
             if(item.name !== name){
@@ -64,8 +80,10 @@ export class Winkelmand extends React.Component<RouteComponentProps<{}>, Winkelm
     }
 
     createStorageAndState(){
-        var game = new codGame("cod");
-        var game2 = new codGame("pubg");
+        var game: BestellingenGame = {name: "GTA V", console: "Xbox", price: 60, category: "Games", orderdate: Date.now(), status:"Onderweg",
+        image: "http://gamesofpc.com/wp-content/uploads/2015/02/GTA-5-Download.jpg"}
+        var game2: BestellingenGame = {name: "GTA V", console: "Xbox", price: 60, category: "Games", orderdate: Date.now(), status:"Onderweg",
+        image: "http://gamesofpc.com/wp-content/uploads/2015/02/GTA-5-Download.jpg"}
 
         localStorage.setItem('winkelmand', JSON.stringify({list: [game, game2] }));
     }
@@ -79,13 +97,14 @@ export class Winkelmand extends React.Component<RouteComponentProps<{}>, Winkelm
     componentWillMount(){
         this.createStorageAndState();
         this.ConvertJson()
-
     }
 
     render() {
         return <div className={"Winkelmand"}> 
 
                 <button onClick={ this.addGameToStorage }> add an item </button>   
+
+                <button onClick={ this.addWinkelmandToBestellingen }> add to bestellingen </button>   
     
                 <h1>Winkelmand {", " + this.state.games.length + " items in Winkelwagen"}</h1> 
                 <h2> {} </h2>
