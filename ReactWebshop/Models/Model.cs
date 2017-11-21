bf7d5084a7654af9f0c10c84ed418d0b38e2cd3f
0870603 <0870603@hr.nl>
2017-11-21 12:43:57 +0100

    Table connections

--- ReactWebshop/Models/Model.cs
+++ ReactWebshop/Models/Model.cs
@@ -26,8 +26,68 @@ namespace Models
 
     public class Wenslijst {
         public int productNmr { get; set; }
-        //public DateTime toevoegDatum
+        public DateTime toevoegDatum { get; set;}
+        public int klantNmr { get; set; }
+        public list<Wenslijst> Wenslijst { get; set; }
 }                          
+    public class Klant {
+        public int klantNmr { get; set; }
+        public string klantNaam { get; set; }
+        public string klantAchternaam { get; set; }
+        public string klantTussenvoegsel { get; set; }
+        public string klantTel { get; set; }
+        public string klantMail { get; set; }
+        public string klantStraat { get; set; }
+        public string klantPostcode { get; set; }
+        public string klantStraatnmr { get; set; }
+        public list<Klant> Klant { get; set; }
+}
+    public class Product {
+        public string productNmr { get; set; }
+        public string productNaam { get; set; }
+        public string productUitgeven { get; set; }
+        public string productOmschr { get; set; }
+        public int aantalInVooraad { get; set; }
+        public decimal productPrijs { get; set; }
+        public string  productType { get; set; }
+        public string productOntwikkelaar { get; set; }
+    }
 
+    public class Werknemer {
+        public int werknemersNmr { get; set; }
+        public string voorNaam { get; set;}
+        public string achterNaam { get; set; }
+        public string tussenVoegsel { get; set; } 
+        public string mail { get; set; }
+        public string functie { get; set; }
+    }
 
+    public class Bestellingen {
+        public int bestellingNmr { get; set; }
+        public DateTime bestellingDatum { get; set; }
+        public DateTime verstuurDatum { get; set; }
+        public string status { get; set; }
+        public string klantNmr { get; set; }
+    }
+
+    public class Betaling {
+        public string klantNmr { get; set; }
+        public string checkNmr { get; set; }
+        public DateTime betalingsDatum { get; set; }
+        public decimal bedrag { get; set; }
+    }
+
+    public class detailsBestelling {
+        public int orderNmr { get; set; }
+        public string productCode { get; set; }
+        public int bestellingAantal { get; set; }
+        public decimal stukPrijs { get; set; }
+    }
+
+    public class productType {
+        public string Type { get; set; }
+        public string omschrijving { get; set; }
+        public string html { get; set; }
+        public string image { get; set; }
+    } 
 }
