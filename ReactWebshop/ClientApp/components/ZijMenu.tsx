3f15a237503425e9a80561c45a0d12693092a7ea
0870603 <0870603@hr.nl>
2017-11-29 11:15:01 +0100

    Testing bootstrap
    zijbalk

--- ReactWebshop/ClientApp/components/ZijMenu.tsx
+++ ReactWebshop/ClientApp/components/ZijMenu.tsx
@@ -1,81 +1,86 @@
 import * as React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-
+import { Nav, NavItem } from 'reactstrap';
 //The navigation menu of the side bar
 
 export class ZijMenu extends React.Component<{}, {}> {
     public render() {
         return <nav className="Zijbalk">
-            <h2>Consoles</h2>
-            <ul>
-            <li>
-                <NavLink to={ '/Consoles/Xbox360' } exact activeClassName='active' className='LinksSide'>
-                   Xbox 360
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Consoles/XboxOne' } exact activeClassName='active'className='LinksSide'>
-                    Xbox One
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Consoles/Playstation3' } exact activeClassName='active' className='LinksSide'>
-                   Playstation 3
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Consoles/Playstation4' } exact activeClassName='active' className='LinksSide'>
-                   Playstation 4
-                </NavLink>
-                </li>
-            </ul>
-            <h2>Games</h2>
-            <ul>
-                <li>
-                <NavLink to={ '/Games/Action' } exact activeClassName='Active'className='LinksSide'>
-                    Action
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Games/Shooter' } exact activeClassName='Avontuur'className='LinksSide'>
-                    Shooter
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Games/Fantasy' } exact activeClassName='active'className='LinksSide'>
-                    Fantasy
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Games/Sport' } exact activeClassName='active'className='LinksSide'>
-                    Sport
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Games/Sandbox' } exact activeClassName='active'className='LinksSide'>
-                    Sandbox
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Games/Fight' } exact activeClassName='active'className='LinksSide'>
-                    Fight
-                </NavLink>
-                </li>
+
+            
+            <div>
+                <h2>Consoles</h2>
+                <nav>
+                    <NavLink to={ '/Consoles/Xbox360' } exact activeClassName='active' className='LinksSide'>
+                       Xbox 360
+                    </NavLink>
+                </nav>
+                <nav>
+                    <NavLink to={ '/Consoles/XboxOne' } exact activeClassName='active'className='LinksSide'>
+                        Xbox One
+                    </NavLink>
+                </nav>
+                <nav>
+                    <NavLink to={ '/Consoles/Playstation3' } exact activeClassName='active' className='LinksSide'>
+                       Playstation 3
+                    
+                    </NavLink>
+                </nav>
+                    <nav>
+                        <NavLink to={ '/Consoles/Playstation4' } exact activeClassName='active' className='LinksSide'>
+                           Playstation 4
+                        </NavLink>
+                    </nav>
+                </div>
+            
+                <div>
+                    <h2>Games</h2>
+                    <nav>
+                        <NavLink to={ '/Games/Action' } exact activeClassName='Active'className='LinksSide'>
+                            Action
+                        </NavLink>
+                    </nav>
+                    <nav>
+                        <NavLink to={ '/Games/Shooter' } exact activeClassName='Avontuur'className='LinksSide'>
+                            Shooter
+                        </NavLink>
+                    </nav>
+                    <nav>
+                        <NavLink to={ '/Games/Fantasy' } exact activeClassName='active'className='LinksSide'>
+                            Fantasy
+                        </NavLink>
+                    </nav>
+                    <nav>
+                        <NavLink to={ '/Games/Sport' } exact activeClassName='active'className='LinksSide'>
+                            Sport
+                        </NavLink>
+                    </nav>
+                    <nav>
+                        <NavLink to={ '/Games/Sandbox' } exact activeClassName='active'className='LinksSide'>
+                            Sandbox
+                        </NavLink>
+                    </nav>
+                    <nav>
+                        <NavLink to={ '/Games/Fight' } exact activeClassName='active'className='LinksSide'>
+                            Fight
+                        </NavLink>
+                    </nav>
                 
-            </ul>
-            <h2>Accessoires</h2>
-            <ul>
-                <li>
-                <NavLink to={ '/Accessoires/Headsets' } exact activeClassName='active'className='LinksSide'>
-                    Headsets
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Accessoires/Racewheels' } exact activeClassName='active'className='LinksSide'>
-                    Race-Wheels
-                </NavLink>
-                </li>
-            </ul>
-        </nav>;
-    }
-}
+                </div>
+            
+            
+                <div>
+                    <h2>Accessoires</h2>
+                    <nav>
+                        <NavLink to={ '/Accessoires/Headsets' } exact activeClassName='active'className='LinksSide'>
+                            Headsets
+                        </NavLink>
+                    </nav>
+                    <nav>
+                        <NavLink to={ '/Accessoires/Racewheels' } exact activeClassName='active'className='LinksSide'>
+                            Race-Wheels
+                        </NavLink>
+                    </nav>
+                </div>
+   </nav>
+     
