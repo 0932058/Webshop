a3f1863db74ce9e55a12fa010183865d1052a7c2
FelixdeJonge <felixdejonge@gmail.com>
2017-11-30 12:59:05 +0100

    Respinsive container

--- ReactWebshop/ClientApp/components/Storage/ShoppingCart/WinkelmandContainer.tsx
+++ ReactWebshop/ClientApp/components/Storage/ShoppingCart/WinkelmandContainer.tsx
@@ -22,66 +22,7 @@ export class Winkelmand extends AbstractStorage {
             <h1>Winkelmand</h1>
             {this.state.products.map((product,index) =>
             <WinkelMandComponent key={index} shoppingCartProduct={product} RemoveItemFromStorage={null}/>)
-            }
-
-            {/* {this.state.convertedStorageProducts.map((storageProduct,index) =>
-            <WinkelMandComponent key={index} shoppingCartProduct={storageProduct} RemoveItemFromStorage={this.RemoveItemFromStorage}/>)  
-            }
-            <p> Total items: {this.state.convertedStorageProducts.length}</p>
-            <p> Total Price: €{this.state.totalPrice.toFixed(2)}</p>
-            <h2> <NavLink to={ '/Afrekenen' } exact activeClassName='active' className='AfrekenLink'> Naar Afrekenen </NavLink> </h2>
-            </div>           */}
-            </div>
+            }    
+        </div>
         )}
 }
-
-{
-    //Old comments
-    
-    /* //localstorage related files
-
-// var games = JSON.parse(String(localStorage.getItem('winkelmand')));
-// var gamesList: game[] = games.list;
-
-// localStorage.setItem('winkelmand', JSON.stringify({list: gamesList }));
-
-// this.ConvertJson();
-// // }
-// ConvertJson() : any{
-//     //hier wordt de value van wenslijst naar string gecast om vervolgens naar een JSON object te worden gecast
-//     var games = JSON.parse(String(localStorage.getItem('winkelmand')));    
-//     if(games != null){
-//         this.setState({gampes: games.list})
-//     }
-// createStorageAndState(){
-//     localStorage.setItem('winkelmand', JSON.stringify({list: []}));
-   
-// }
-// addWinkelmandToBestellingen() {
-//     // localStorage.setItem('bestellingen', JSON.stringify({list: []}));
-//     // var bestellingen = JSON.parse(String(localStorage.getItem('bestellingen')));
-//     // var winkelmand = JSON.parse(String(localStorage.getItem('winkelmand')));
-
-//     // var winkelmandList: BestellingenGame[] = winkelmand.list;
-//     // var bestellingenList: BestellingenGame[] = bestellingen.list;
-    
-//     // winkelmandList.map(item => {
-//     //     bestellingenList.push(item);
-//     // });
-
-//     // localStorage.setItem('bestellingen', JSON.stringify({list: bestellingenList}));
-
-//     // localStorage.setItem('winkelmand', JSON.stringify({list: []}));
-//     // this.ConvertJson();
-
-            //If the user is not logged in then this code below will be activated
-        // var wenslijst = JSON.parse(String(localStorage.getItem('winkelmand')));
-
-        // var wenslijstList: game[] = wenslijst.list;
-        // if(this.state.game != null){
-        //     wenslijstList.push(this.state.game);
-        // }
-        
-
-        // localStorage.setItem('winkelmand', JSON.stringify({list: wenslijstList}));
-// */}
