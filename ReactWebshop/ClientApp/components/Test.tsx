fa53ba100485b0e8d958cdf315b4058b2fe4aa38
0870603hr <0870603@hro.nl>
2017-11-30 11:13:05 +0100

    terugzetten van oude files

--- ReactWebshop/ClientApp/components/Test.tsx
+++ ReactWebshop/ClientApp/components/Test.tsx
@@ -1,11 +1,27 @@
+import { configureLoadStyles } from '@microsoft/load-themed-styles';
+
+// Store registered styles in a variable used later for injection.
+let _allStyles = '';
+
+// Push styles into variables for injecting later.
+configureLoadStyles((styles: string) => {
+  _allStyles += styles;
+});
+
 import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
+import * as ReactDOMServer from 'react-dom/server';
+import { Button } from 'office-ui-fabric-react/lib/Button';
 
-//This component can be used for tests
+let body = ReactDOMServer.renderToString(<Button>hello</Button>);
 
-export class Test extends React.Component<{}, {}> { 
-    public render() {
-        return <div> 
-    )} </div>
-    }
-}
+console.log(
+  `
+  <html>
+  <head>
+    <style>${ _allStyles}</style>
+  </head>
+  <body>
+    ${ body}
+  </body>
+  </html>
+  `);
