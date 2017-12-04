9b5e3b816f850f1b7a8331cba77d1a349da02d17
FelixdeJonge <felixdejonge@gmail.com>
2017-12-04 10:09:01 +0100

    Winkelmand af

--- ReactWebshop/ClientApp/components/Storage/ShoppingCart/WinkelmandContainer.tsx
+++ ReactWebshop/ClientApp/components/Storage/ShoppingCart/WinkelmandContainer.tsx
@@ -13,6 +13,46 @@ export class Winkelmand extends AbstractStorage {
         super();
         this.state = {customerID: null, isShoppingCart:true, loaded:false, totalPrice: this.GetTotalPrice(), products: this.GetCartContent()}
     }
+    /*
+    BuildComponents(){
+        var cart = this.GetCartContent()
+        if (cart == []){
+            return <div>
+                <h2>Uw winkelmand is leeg.</h2>
+            </div>
+        }
+        else{
+            var res;
+            cart.forEach(product =>{
+                res += 
+                <div className={"Component"}>
+                    <h1>{product.name}</h1>
+                    <img src={product.image}/>
+                    <div> 
+                        <h2> Naam: {product.name} </h2>
+                        <h2> Prijs: {"€" + product.price.toFixed(2)} </h2>
+                        <h2> <button onClick={() => this.RemoveItemFromStorage(product.index)}> Verwijderen </button> </h2>        
+                    </div>
+                </div>
+
+            });
+            return res
+        }
+    }
+    */    
+    RemoveItemFromStorage(id){
+        var oldlist = [];
+        var newlist = [];
+        oldlist = JSON.parse(localStorage.getItem("Winkelmand"));
+        oldlist.forEach(item =>{
+            if (item.index != id){
+                newlist.push(item);
+            }
+        });
+        localStorage.setItem("Winkelmand" , JSON.stringify(newlist));
+        this.setState({totalPrice: this.GetTotalPrice(), products: this.GetCartContent()});
+
+    }
     GetCartContent(){
         var cart = [];
         cart = JSON.parse(localStorage.getItem("Winkelmand"));
@@ -40,13 +80,13 @@ export class Winkelmand extends AbstractStorage {
     AddTestItemToStorage(){
         var itemlist = this.state.products;
         if (itemlist != null){
-            var item = {"name" : "Vidja", "id" : 1, "price": 60, "index" : itemlist.length};
+            var item = {"name" : "Vidja", "id" : 1, "price": 60, "index" : itemlist.length, "console": "PoTatOS"};
             itemlist.push(item)
             localStorage.setItem("Winkelmand", JSON.stringify(itemlist));
             this.setState({totalPrice: this.GetTotalPrice(), products: this.GetCartContent()});
         }
         else{
-            var item = {"name" : "Vidja", "id" : 1, "price": 60, "index" : 0};
+            var item = {"name" : "Vidja", "id" : 1, "price": 60, "index" : 0, "console": "PoTatOS"};
             itemlist = [item]
             localStorage.setItem("Winkelmand", JSON.stringify(itemlist));
             this.setState({totalPrice: this.GetTotalPrice(), products: this.GetCartContent()});
@@ -60,8 +100,27 @@ export class Winkelmand extends AbstractStorage {
         <div className={"Container"}>
                 <h1>Winkelmand</h1>
                 <div>
-                {this.state.products.map((storageProduct,index) =>
-                <WinkelMandComponent key={index} shoppingCartProduct={storageProduct} RemoveItemFromStorage={null}/>)  
+                {
+                    this.state.products != []?
+                        this.state.products.map(
+                            product =>{
+                                return(
+                                    <div className={"Component"}>
+                                        <h1>{product.name}</h1>
+                                        <img src={product.image}/>
+                                        <div> 
+                                            <h2> Naam: {product.name} </h2>
+                                            <h2> Console: {product.console}</h2>
+                                            <h2> Prijs: {"€" + product.price.toFixed(2)} </h2>
+                                            <h2> <button onClick={() => this.RemoveItemFromStorage(product.index)}> Verwijderen </button> </h2>        
+                                        </div>
+                                    </div>
+                                )
+                            }
+
+                        )
+                        :
+                        <h2>Uw winkelmand is leeg</h2>
                 }
                 </div>
                 <h2> Aantal producten: {this.state.products.length}</h2>
