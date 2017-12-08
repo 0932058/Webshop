4ac7aab12799cace3259aa728a82a012b86a2f6f
MikaGrootenboer <0925620@hr.nl>
2017-12-08 14:53:46 +0100

    Layout fixes

--- ReactWebshop/ClientApp/components/Profile/Layout/ProfileSideMenuLinks.tsx
+++ ReactWebshop/ClientApp/components/Profile/Layout/ProfileSideMenuLinks.tsx
@@ -7,24 +7,9 @@ import { RouteComponentProps } from 'react-router';
 export class ProfileSideMenuLinks extends React.Component<{}, {}> {
     public render() {
         return <nav className="ProfileSideMenuLinks">
-            <h2>Profiel</h2>
-            <ul>
-            <li>
-                <NavLink to={ '/Profile/Gegevens' } exact activeClassName='active' className='LinksSide'>
-                   Gegevens
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Winkelmand' } exact activeClassName='active'className='LinksSide'>
-                    Winkelmand
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to={ '/Wenslijst' } exact activeClassName='active' className='LinksSide'>
-                   Wenslijst
-                </NavLink>
-                </li>
-            </ul>
+                    <h2>Profiel</h2>
+                    <div className="list-group">
+                    </div>
         </nav>;
     }
 }
