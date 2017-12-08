427bf03044a2f7e59475c602883700898676a536
MikaGrootenboer <0925620@hr.nl>
2017-12-08 14:54:11 +0100

    profiel mooi gemaakt

--- ReactWebshop/ClientApp/components/Profile/Layout/ProfileSideMenuLinks.tsx
+++ ReactWebshop/ClientApp/components/Profile/Layout/ProfileSideMenuLinks.tsx
@@ -9,6 +9,9 @@ export class ProfileSideMenuLinks extends React.Component<{}, {}> {
         return <nav className="ProfileSideMenuLinks">
                     <h2>Profiel</h2>
                     <div className="list-group">
+                        <a href="/Profile/Gegevens" className="list-group-item"><span className="glyphicon glyphicon-user">  Gegevens</span></a>
+                        <a href="/Winkelmand" className="list-group-item"><span className="glyphicon glyphicon-shopping-cart">  Winkelmand</span></a>
+                        <a href="/Wenslijst" className="list-group-item"><span className='glyphicon glyphicon-heart'>   Wenslijst</span></a>
                     </div>
         </nav>;
     }
